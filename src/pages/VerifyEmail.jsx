95fe1063b66133b77864a1bc519a4a708088bb46
unknown <yusuff4u2c@gmail.com>
2024-11-28 22:39:02 +0100

    navigation

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import api from "../data/api";
 import toast from "react-hot-toast";
 import { set } from "react-hook-form";
@@ -6,7 +6,7 @@ import { set } from "react-hook-form";
 function VerifyEmail() {
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
-  const location = useLocation();
+  const navigate = useNavigate();
 
   api
     .get("/api/auth/verify_email?token=" + token)
@@ -14,7 +14,7 @@ function VerifyEmail() {
       if (response.data.success) {
         toast.success("Email verified successfully");
         setTimeout(() => {
-          location.navigate("/auth/login");
+          navigate("/auth/login");
         }, 2000);
       } else if (
         !response.data.success &&
@@ -22,12 +22,12 @@ function VerifyEmail() {
       ) {
         toast.error("Email already verified");
         setTimeout(() => {
-          location.navigate("/auth/login");
+          navigate("/auth/login");
         }, 2000);
       } else {
         toast.error("Email verification failed");
         setTimeout(() => {
-          location.navigate("/auth/login");
+          navigate("/auth/login");
         }, 2000);
       }
     })
